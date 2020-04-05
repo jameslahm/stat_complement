6586bdc28e7ad34946f7dfdd1877e39afb3fb991
wangao <1366463855@qq.com>
2020-04-05 19:33:58 +0800

    feat: add sor

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
  
 ### Usage
 ```python
-from stat_complement import HotellingT2,JaccobiSolve
+from stat_complement import HotellingT2,JacobiSolve
 
 X=np.array([[-3,15],[4,16],[5,11]])
 mu=np.array([[4,14]])
 HotellingT2(X,mu)
 
 # x: initial point delta: stop criteria
-JaccobiSolve(A,b,x,delta)
+JacobiSolve(A,b,x,delta)
 ```
 
 You are welcome to put up some function about statistic computation and I will try best to add it
--- stat/main.py
+++ stat/main.py
@@ -8,7 +8,7 @@ def HotellingT2(X, mu):
     return n*(mean-mu).dot(np.linalg.inv(S)).dot((mean-mu).T)
 
 
-def JaccobiSolve(A, b, x,delta):
+def JacobiSolve(A, b, x,delta):
     L = np.zeros(len(A), len(A))
     for i in range(len(A)):
         L[i][i] = A[i][i]
@@ -18,4 +18,15 @@ def JaccobiSolve(A, b, x,delta):
         if(max(abs(xn-x)) < delta):
             break
         x = xn
+    return x
+
+def sor(A,b,w,x):
+    while(1):
+        temp=x.copy()
+        for i in range(0,len(x)):
+            x[i]=(1-w)*x[i]+w*(b[i]-sum([A[i][j]*x[j] for j in range(0,i)])-sum([A[i][j]*x[j] for j in range(i+1,len(x))]))/A[i][i]
+        print(x)
+        print(temp)
+        if(max(abs(temp-x))<10**(-2)):
+            break
     return x
